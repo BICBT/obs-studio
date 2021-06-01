bfaff3c1b2024e6ff9badb58d747083ef689bc42
Meng Li <Mengli619@github.com>
2021-06-01 15:04:29 +0800

    show timestamp

--- libobs/util/font-rasterizer.h
+++ libobs/util/font-rasterizer.h
@@ -5,11 +5,25 @@
 extern "C" {
 #endif
 
-EXPORT bool font_rasterizer_initialize(const char *fontPath);
+struct font_rasterizer_bitmap {
+	uint8_t *data;
+	uint32_t width;
+	uint32_t height;
+};
+
+EXPORT bool font_rasterizer_initialize(const char *fontPath,
+				       uint32_t fontHeight);
 
 EXPORT void font_rasterizer_uninitialize();
 
-EXPORT void font_rasterize(const char *text, int width, int height, unsigned char* bitmap);
+EXPORT void
+font_rasterizer_create_bitmap(int width, struct font_rasterizer_bitmap *bitmap);
+
+EXPORT void
+font_rasterizer_destroy_bitmap(struct font_rasterizer_bitmap *bitmap);
+
+EXPORT void font_rasterize(const char *text,
+			   struct font_rasterizer_bitmap *bitmap);
 
 #ifdef __cplusplus
 }
